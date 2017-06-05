7f3b22ee84b1bbdf11e9c5cf5e1407d862c2d6c7
mohanlalkrish <mohanlal051997@gmail.com>
2017-06-05 05:15:40 -0700

    Update and rename greatestno.java to greatest.java

---
+++ greatest.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+class greatest
+{
+public static void main(String[] args) 
+{
+int a,b,c,d;
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+int b=s.nextInt();
+int c=s.nextInt();
+int d=Math.max(a,b);
+System.out.println(Math.max(d,c));
+}
+}
