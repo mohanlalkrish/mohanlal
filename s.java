906760e0e4cab30f5e83528adf6090accbec466a
mohanlalkrish <mohanlal051997@gmail.com>
2017-06-06 01:54:07 -0700

    Create s.java

---
+++ s.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class s
+{
+    public static void main(String arg[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int sum = 0;
+        
+        for(int i = 1; i <= a; i++)
+        {
+            sum = sum + i;    
+            
+        }
+ System.out.println("Sum is : " + sum);
+    
+    }
+}
