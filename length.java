3aa9396853143d41b3b7195e8661d5e73bb43e8f
mohanlalkrish <mohanlal051997@gmail.com>
2017-06-06 02:00:28 -0700

    Create length.java

---
+++ length.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class length
+{
+    public static void main(String arg[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int l=(int)(Math.log10(a)+1);
+ System.out.println(l);
+    
+    }
+}
