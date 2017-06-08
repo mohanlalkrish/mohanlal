61d7c3e9cb7b62c771c12d42fac65962c10a5231
mohanlalkrish <mohanlal051997@gmail.com>
2017-06-08 04:46:09 -0700

    Create str.java

---
+++ str.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class str
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String ss=s.nextLine();
+StringBuffer sb=new StringBuffer(ss);
+System.out.println(sb.reverse());
+}}
