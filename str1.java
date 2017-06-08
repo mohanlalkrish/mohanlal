2845dbf3d3975d93313aa6578f5fb1b5878f9460
mohanlalkrish <mohanlal051997@gmail.com>
2017-06-08 05:13:44 -0700

    Create str1.java

---
+++ str1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class str1
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int i=s.nextInt();
+String ss=String.valueOf(i);
+StringBuffer sb=new StringBuffer(ss);
+System.out.println(sb.reverse());
+}}
